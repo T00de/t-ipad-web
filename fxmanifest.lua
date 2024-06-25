e9d013768718d2e7ae77c74bddd03f6b80faffa4
Tode <159941841+T00de@users.noreply.github.com>
2024-06-25 10:12:57 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Tode Development'
+description 'Website in a Ipad!'
+version '1.0.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/ipad_ram.png'
+}
